82ff18cb2e14e691c507f8b4209de14854123485
pyCross <35365506+stealthybastard@users.noreply.github.com>
2025-02-25 16:22:30 +0100

    Movement
    Camera movement and such
    
    Also esc now closes the game

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,14 +4,23 @@ using System;
 public partial class PlayerController : CharacterBody3D
 {
 	[Export]
-	public int Speed { get; set; } = 14;
+	public int speed { get; set; } = 14;
 	[Export]
 	public float gravity { get; set; } = 16;
-	private Vector3 _targetVelocity = Vector3.Zero;
-
+	
+	[Export(PropertyHint.Range, "0.f,1.f,0.01f")]
+	float cameraSensitivity = 0.01f;
+	[Export(PropertyHint.Range, "0.f,360.f,1.f,radians_as_degrees")]
+	float cameraTiltLimit = Mathf.DegToRad(75);
+	
+	private Vector3 targetVelocity = Vector3.Zero;
+	private Camera3D camera;
+	private Node3D cameraPivot;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		cameraPivot = GetNode<Node3D>("cameraPivot");
+		camera = GetNode<Camera3D>("cameraPivot/cameraArm/playerCamera");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,54 +31,81 @@ public partial class PlayerController : CharacterBody3D
 	public override void _PhysicsProcess(double delta)
 	{
 		var direction = Vector3.Zero;
+		
+		Vector3 camRot = camera.GetGlobalRotation();
+		
+		Vector3 forward = new Vector3(-(float)Mathf.Sin(camRot.Y), 0, -(float)Mathf.Cos(camRot.Y));
+		Vector3 right = new Vector3(-(float)Mathf.Sin(camRot.Y - (Math.PI * 0.5f)), 0, -(float)Mathf.Cos(camRot.Y - (Math.PI * 0.5f)));
 
 		if (Input.IsActionPressed("move_right"))
 		{
-			direction.X += 1.0f;
+			direction += right;
 		}
 		if (Input.IsActionPressed("move_left"))
 		{
-			direction.X -= 1.0f;
+			direction -= right;
 		}
 		if (Input.IsActionPressed("move_back"))
 		{
-			direction.Z += 1.0f;
+			direction -= forward;
 		}
 		if (Input.IsActionPressed("move_forward"))
 		{
-			direction.Z -= 1.0f;
+			direction += forward;
 		}
 
 		if (direction != Vector3.Zero)
 		{
 			direction = direction.Normalized();
 			// Setting the basis property will affect the rotation of the node.
-			GetNode<Node3D>("Pivot").Basis = Basis.LookingAt(direction);
+			GetNode<Node3D>("meshPivot").Basis = Basis.LookingAt(direction);
 		}
 
-		_targetVelocity.X = direction.X * Speed;
-		_targetVelocity.Z = direction.Z * Speed;
+		targetVelocity.X = direction.X * speed;
+		targetVelocity.Z = direction.Z * speed;
 
 		if (IsOnFloor())
 		{
-			if (_targetVelocity.Y < 0.1f)
+			if (targetVelocity.Y < 0.1f)
 			{
-				_targetVelocity.Y = 0;
+				targetVelocity.Y = 0;
 			}
 			if (Input.IsActionPressed("jump"))
 			{
-				_targetVelocity.Y = 5;
+				targetVelocity.Y = 5;
 			}
 		}
 		else
 		{
-			_targetVelocity.Y -= gravity * (float)delta;
+			targetVelocity.Y -= gravity * (float)delta;
 		}
 
-		Velocity = _targetVelocity;
+		Velocity = targetVelocity;
 
 		if (MoveAndSlide())
 			HandleCollision();
+	}
+	
+	public override void _UnhandledInput(InputEvent @event) //0 clue what the @ means ¯\_(ツ)_/¯  TODO: make proper input events
+	{
+		if (@event is InputEventKey keyEvent)
+		{
+			if (keyEvent.Pressed && keyEvent.Keycode == Key.Escape)
+			{
+				GetTree().Quit();
+			}
+		}
+		
+		if (@event is InputEventMouseMotion mouseEvent)
+		{
+			Vector3 cameraRot = cameraPivot.GetRotation();
+			
+			cameraRot.X -= mouseEvent.GetRelative().Y * cameraSensitivity;
+			cameraRot.X = Mathf.Clamp(cameraRot.X, -cameraTiltLimit, cameraTiltLimit);
+			cameraRot.Y -= mouseEvent.GetRelative().X * cameraSensitivity;
+			
+			cameraPivot.SetRotation(cameraRot);
+		}
 	}
 
 	private void HandleCollision()
